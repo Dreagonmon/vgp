d5f0948761d9fdda039d5ae7fcf2d2c0a5230e06
Dreagonmon <531486058@qq.com>
2023-07-30 22:19:55 +0800

    add more screen function

--- test_sample/assembly/index.ts
+++ test_sample/assembly/index.ts
@@ -1,11 +1,10 @@
 // The entry file of your WebAssembly module.
 import {
-  getFeature,
   cpu_ticks_ms,
   ticks_diff,
 } from "./env.ts";
 import {
-  fill_screen,
+  full_screen_text,
   get_screen_height,
   get_screen_width,
   screen_init,
@@ -21,18 +20,16 @@ export function vinit(): void {
   SCRH = get_screen_height();
   console.log(`Screen Width: ${SCRW}`);
   console.log(`Screen Height: ${SCRH}`);
-  let i = 0;
+  let i: i32 = 0;
   while (i < 999999) {
     i++;
   }
-  fill_screen(1);
   const etime: i32 = cpu_ticks_ms();
   console.log(`number: ${i}`);
   console.log(`inited. CPU time: ${ticks_diff(etime, stime)} ms`);
+  full_screen_text(`Virtual Game\nPocket\n\nBoot: ${ticks_diff(etime, stime)}ms`);
 }
 
 export function vloop(): void {
-  // console.log("vloop");
-  // console.log("filling screen...");
-  // abort();
+  // full_screen_text("Hello\nDragon~");
 }
