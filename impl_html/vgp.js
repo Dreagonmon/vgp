ec2785a6edd99b0ae3a0523bb1b0f3b9897ed949
Dreagonmon <531486058@qq.com>
2023-10-01 18:42:52 +0800

    basic html runtime implement

--- impl_html/vgp.js
+++ impl_html/vgp.js
@@ -35,20 +35,51 @@ const TRUE = 1;
 const FALSE = 0;
 
 const BACKLOG_LIMIT = 9999;
+const LKEY_SAVE_PREFIX = "_vgps_";
+const LKEY_RTC_OFFSET = "_vrtc_";
+
+const as_u32 = (num) => {
+    if (typeof num === "bigint") {
+        return Number.parseInt(num & 0xFFFFFFFFn);
+    } else if (typeof num === "number") {
+        return num & 0xFFFFFFFF;
+    }
+    return num;
+};
+
+const as_i32 = (num) => {
+    if (typeof num === "bigint") {
+        return Number.parseInt(num % 0x80000000n);
+    } else if (typeof num === "number") {
+        return num % 0x80000000;
+    }
+    return num;
+};
+
+const u32_as_i32 = (num) => {
+    let n = as_u32(num);
+    if (n > 0x7FFF_FFFF) {
+        n = n - 0xFFFF_FFFF - 1;
+    }
+    return n;
+};
+
+const i32_as_u32 = (num) => {
+    let n = as_i32(num);
+    if (n < 0) {
+        n = n + 0xFFFFFFFF + 1;
+    }
+    return n;
+};
 
 export class VirtualGamePocket {
+    // ======== display related ========
     #scrw = 0; // screen width
     #scrh = 0; // screen height
     #savcap = 0; // save capacity
     #pscale = 1; // pixel scale mod
     #pofmod = 0; // pixel offset mod
     #pszmod = 0; // pixel size mod
-    /** @type {HTMLElement} */
-    #pelem = null; // print element
-    /** @type {Array<string>} */
-    #phis = []; // print history
-    /** @type {Array<string>} */
-    #pcline = []; // print current line
     /** @type {HTMLCanvasElement} */
     #ofscvs = null; // off screen canvas
     /** @type {HTMLCanvasElement} */
@@ -56,15 +87,35 @@ export class VirtualGamePocket {
     /** @type {Uint8Array} */
     #dspbuf = null; // display buffer
     #dspbufdty = false; // display buffer is dirty
+    //
+    // ======== print related ========
+    /** @type {HTMLElement} */
+    #pelem = null; // print element
+    /** @type {Array<string>} */
+    #phis = []; // print history
+    /** @type {Array<string>} */
+    #pcline = []; // print current line
+    //
+    // ======== gamepad related ========
+    #kstat = 0; // key status
+    //
+    // ======== save related ========
     #name = "VGP"; // name used for prefix
     /** @type {Uint8Array} */
     #savbuf = null; // save buffer
-    #exited = false; // exit flag
+    //
+    // ======== rtc related ========
+    #rtcoff = 0n; // rtc offset
+    #rtcval = 0n; // rtc value
+    #rtcmod = 0n; // rtc value (modified)
+    #rtcdty = false; // rtc is modified
+    //
+    // ======== runtime related ========
     /** @type {WebAssembly.Instance} */
     #wasm = null; // wasm instance
     /** @type {WebAssembly.Memory} */
     #mem = null; // wasm memory
-    #kstat = 0; // key status
+    #exited = false; // exit flag
     constructor (screenWidth = 128, screenHeight = 64, saveCapacity = 8192, pixelScale = 8, pixelOffsetMod = 1, pixelSizeMod = -2) {
         this.#scrw = screenWidth;
         this.#scrh = screenHeight;
@@ -73,10 +124,19 @@ export class VirtualGamePocket {
         this.#pofmod = pixelOffsetMod;
         this.#pszmod = pixelSizeMod;
         this.#ofscvs = document.createElement("canvas");
-        this.#ofscvs.width = this.#scrw * this.#pscale;
-        this.#ofscvs.height = this.#scrh * this.#pscale;
         this.#dspbuf = new Uint8Array(this.#scrw * (Math.ceil(this.#scrh / 8)));
         this.#mem = new WebAssembly.Memory({ initial: 1, maximum: undefined });
+        // init some varbs
+        this.#ofscvs.width = this.#scrw * this.#pscale;
+        this.#ofscvs.height = this.#scrh * this.#pscale;
+        const ctx = this.#ofscvs.getContext("2d");
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, this.#ofscvs.width, this.#ofscvs.height);
+        try {
+            this.#rtcoff = BigInt(localStorage.getItem(LKEY_RTC_OFFSET));
+        } catch {
+            this.#rtcoff = 0n;
+        }
     }
 
     #putChar (ch) {
@@ -89,6 +149,9 @@ export class VirtualGamePocket {
         if (chString === "\n") {
             const line = this.#pcline.join("");
             this.#phis.push(line);
+            if (this.#phis.length > BACKLOG_LIMIT) {
+                this.#phis.splice(0, 1);
+            }
             this.#pcline.splice(0, this.#pcline.length);
             if (this.#pelem != null) {
                 this.#pelem.innerText = this.#phis.join("\n");
@@ -111,25 +174,37 @@ export class VirtualGamePocket {
         this.#putString(`[VGP Error] ${text}`, true);
     }
 
-    bindPrintElement (elem) {
-        this.#pelem = elem;
+    #about (reason) {
+        this.#putError(reason);
+        this.#exited = true;
     }
 
-    #impl_get_feature (feature_id) {
-        switch (feature_id) {
-            case VFEATURE_SCREEN_SIZE:
-                return ((this.#scrw & 0xFFF) << 12) | (this.#scrh & 0xFFF);
-            case VFEATURE_SCREEN_COLOR_FORMAT:
-                return VCOLOR_FORMAT_MVLSB;
-            case VFEATURE_GAMEPAD_SUPPORT:
-                return TRUE;
-            case VFEATURE_SAVE_CAPACITY:
-                // return this.#savcap;
-                return 0;
-            case VFEATURE_RTC_SUPPORT:
-                // return TRUE;
-                return FALSE;
-            default: return INT32_MIN;
+    #getPixelFromBuffer (x, y) {
+        // MVLSB format
+        const index = (y >> 3) * this.#scrw + x;
+        const offset = y & 0x07;
+        const value = (this.#dspbuf[ index ] >> offset) & 0x01;
+        return value;
+    }
+
+    #drawPixel (ctxp, x, y, c) {
+        /** @type {CanvasRenderingContext2D} */
+        const ctx = ctxp;
+        const rect_x = x * this.#pscale + this.#pofmod;
+        const rect_y = y * this.#pscale + this.#pofmod;
+        const rect_w = this.#pscale + this.#pszmod;
+        const rect_h = this.#pscale + this.#pszmod;
+        ctx.fillStyle = c ? "white" : "black";
+        ctx.fillRect(rect_x, rect_y, rect_w, rect_h);
+    }
+
+    #renderOffScreenCanvas () {
+        const ctx = this.#ofscvs.getContext("2d");
+        for (let y = 0; y < this.#scrh; y++) {
+            for (let x = 0; x < this.#scrw; x++) {
+                const color = this.#getPixelFromBuffer(x, y);
+                this.#drawPixel(ctx, x, y, color);
+            }
         }
     }
 
@@ -142,21 +217,61 @@ export class VirtualGamePocket {
         return buffer;
     }
 
-    #impl_call0 (function_id) {
+    #ensureSave () {
+        if (this.#savbuf == null) {
+            this.#savbuf = new Uint8Array(this.#savcap);
+            const localSave = localStorage.getItem(LKEY_SAVE_PREFIX + this.#name);
+            if (localSave) {
+                const copyLimit = Math.min(this.#savcap, localSave.length);
+                for (let i = 0; i < copyLimit; i++) {
+                    this.#savbuf[ i ] = localSave.charCodeAt(i);
+                }
+            }
+        }
+    }
+
+    #impl_get_feature (feature_id) {
         switch (feature_id) {
+            case VFEATURE_SCREEN_SIZE:
+                return ((this.#scrw & 0xFFF) << 12) | (this.#scrh & 0xFFF);
+            case VFEATURE_SCREEN_COLOR_FORMAT:
+                return VCOLOR_FORMAT_MVLSB;
+            case VFEATURE_GAMEPAD_SUPPORT:
+                return TRUE;
+            case VFEATURE_SAVE_CAPACITY:
+                return this.#savcap;
+            // return 0;
+            case VFEATURE_RTC_SUPPORT:
+                return TRUE;
+            // return FALSE;
+            default: return INT32_MIN;
+        }
+    }
+
+    #impl_call0 (function_id) {
+        switch (function_id) {
             case VFUNC_SYSTEM_EXIT:
                 this.#exited = true;
+                this.#putString("[VGP Exit]", true);
                 return 0;
             case VFUNC_CPU_TICKS_MS:
-                return Math.floor(Date.now()) & INT32_MAX;
+                return Math.floor(performance.now()) & INT32_MAX;
             case VFUNC_GAMEPAD_STATUS:
-                return this.#kstat;
+                return u32_as_i32(this.#kstat);
+            case VFUNC_SAVE_FLUSH:
+                this.#ensureSave();
+                localStorage.setItem(LKEY_SAVE_PREFIX + this.#name, String.fromCharCode(...this.#savbuf));
+                return 0;
+            case VFUNC_RTC_GET_H32:
+                return u32_as_i32((this.#rtcmod >> 32n) & 0xFFFF_FFFFn);
+            case VFUNC_RTC_GET_L32:
+                return u32_as_i32(this.#rtcmod & 0xFFFF_FFFFn);
             default: return 0;
         }
     }
 
     #impl_call1 (function_id, p1) {
-        switch (feature_id) {
+        switch (function_id) {
             case VFUNC_UPDATE_SCREEN_BUFFER:
                 const mem = this.#getMemoryBuffer();
                 if (mem) {
@@ -164,8 +279,7 @@ export class VirtualGamePocket {
                     const memSize = mem8.byteLength;
                     const remainSize = memSize - p1;
                     if (p1 < 0 || remainSize < this.#dspbuf.length) {
-                        this.#putError("Display Buffer Pointer Out of Range.");
-                        this.#exited = true;
+                        this.#about(`Display Buffer Pointer Out of Range: ${p1}`);
                         return 0;
                     }
                     for (let i = 0; i < this.#dspbuf.length; i++) {
@@ -177,6 +291,39 @@ export class VirtualGamePocket {
             case VFUNC_TRACE_PUT_CHAR:
                 this.#putChar(p1 & 0xFF);
                 return 0;
+            case VFUNC_SAVE_READ:
+                p1 = i32_as_u32(p1);
+                if (p1 < 0 || p1 >= this.#savcap) {
+                    this.#about(`Save Wrtie Offset Out Of Range: ${p1}`);
+                    return 0;
+                }
+                this.#ensureSave();
+                return u32_as_i32(this.#savbuf[ p1 ]);
+            case VFUNC_RTC_SET_H32:
+                p1 = i32_as_u32(p1);
+                this.#rtcmod = (this.#rtcmod & 0x0000_0000_FFFF_FFFFn) | (BigInt(p1) << 32n);
+                this.#rtcdty = true;
+                return 0;
+            case VFUNC_RTC_SET_L32:
+                p1 = i32_as_u32(p1);
+                this.#rtcmod = (this.#rtcmod & 0xFFFF_FFFF_0000_0000n) | BigInt(p1);
+                this.#rtcdty = true;
+                return 0;
+            default: return 0;
+        }
+    }
+
+    #impl_call2 (function_id, p1, p2) {
+        switch (function_id) {
+            case VFUNC_SAVE_WRITE:
+                p1 = i32_as_u32(p1);
+                if (p1 < 0 || p1 >= this.#savcap) {
+                    this.#about(`Save Wrtie Offset Out Of Range: ${p1}`);
+                    return 0;
+                }
+                this.#ensureSave();
+                this.#savbuf[ p1 ] = p2 & 0xFF;
+                return 0;
             default: return 0;
         }
     }
@@ -184,11 +331,10 @@ export class VirtualGamePocket {
     #makeImportObject () {
         return {
             env: {
-                memory: this.#mem,
                 get_feature: this.#impl_get_feature.bind(this),
                 call0: this.#impl_call0.bind(this),
                 call1: this.#impl_call1.bind(this),
-                call2: (f, p1, p2) => 0,
+                call2: this.#impl_call2.bind(this),
                 call3: (f, p1, p2, p3) => 0,
                 call4: (f, p1, p2, p3, p4) => 0,
             },
@@ -196,12 +342,148 @@ export class VirtualGamePocket {
     }
 
     async load (buffer, name) {
-        await WebAssembly.instantiate(buffer, this.#makeImportObject());
-        this.#name = name;
+        try {
+            const mod = await WebAssembly.compile(buffer);
+            const imps = WebAssembly.Module.imports(mod);
+            const importObject = this.#makeImportObject();
+            for (const item of imps) {
+                if (item.kind == "memory") {
+                    if (!importObject[ item.module ]) {
+                        importObject[ item.module ] = {};
+                    }
+                    importObject[ item.module ] = {
+                        ...importObject[ item.module ],
+                        [ item.name ]: this.#mem,
+                    };
+                }
+            }
+            this.#wasm = await WebAssembly.instantiate(mod, importObject);
+            this.#name = name;
+        } catch (e) {
+            this.#about(e.toString());
+        }
     }
 
-    async loadStream (stream, name) {
-        await WebAssembly.instantiateStreaming(stream, this.#makeImportObject());
-        this.#name = name;
+    async loadHttp (urlText) {
+        try {
+            const url = new URL(urlText, window.location.toString());
+            const name = url.pathname.substring(url.pathname.lastIndexOf("/") + 1);
+            const resp = await fetch(name, {
+                mode: "cors",
+                credentials: "omit",
+            });
+            if (resp.status !== 200) {
+                throw new Error(`Failed to fetch .wasm binary: ${url.toString()}`);
+            }
+            const data = await resp.arrayBuffer();
+            await this.load(data, name);
+        } catch (e) {
+            this.#about(e.toString());
+        }
+    }
+
+    vinit () {
+        try {
+            if (this.#wasm && this.#wasm.exports._initialize && !this.#exited) {
+                this.#wasm.exports._initialize();
+            }
+            if (this.#wasm && this.#wasm.exports._start && !this.#exited) {
+                this.#wasm.exports._start();
+            }
+            if (this.#wasm && this.#wasm.exports.vinit && !this.#exited) {
+                this.#wasm.exports.vinit();
+            }
+        } catch (e) {
+            this.#about(e.toString());
+        }
+    }
+
+    vloop () {
+        try {
+            if (this.#wasm && this.#wasm.exports.vloop && !this.#exited) {
+                this.#wasm.exports.vloop();
+            }
+        } catch (e) {
+            this.#about(e.toString());
+        }
+    }
+
+    doBetweenFrames () {
+        if (this.#dspbufdty) {
+            this.#renderOffScreenCanvas();
+            if (this.#dspcvs) {
+                this.#dspcvs.getContext("2d").drawImage(this.#ofscvs, 0, 0);
+            }
+            this.#dspbufdty = false;
+        }
+        if (this.#rtcdty) {
+            this.#rtcoff = this.#rtcmod - this.#rtcval;
+            localStorage.setItem(LKEY_RTC_OFFSET, this.#rtcoff.toString());
+            this.#rtcdty = false;
+        }
+        this.#rtcval = BigInt(Math.floor(Date.now() / 1000));
+        this.#rtcmod = this.#rtcval + this.#rtcoff;
+    }
+
+    bindPrintElement (elem) {
+        this.#pelem = elem;
+    }
+
+    bindDisplayElement (canvas) {
+        this.#dspcvs = canvas;
+        this.#dspcvs.width = this.#scrw * this.#pscale;
+        this.#dspcvs.height = this.#scrh * this.#pscale;
+    }
+
+    setKeyUpStatus (isPressed) {
+        if (isPressed) {
+            this.#kstat = this.#kstat | KEY_MASK_UP;
+        } else {
+            this.#kstat = this.#kstat & ~(KEY_MASK_UP);
+        }
+    }
+
+    setKeyDownStatus (isPressed) {
+        if (isPressed) {
+            this.#kstat = this.#kstat | KEY_MASK_DOWN;
+        } else {
+            this.#kstat = this.#kstat & ~(KEY_MASK_DOWN);
+        }
+    }
+
+    setKeyLeftStatus (isPressed) {
+        if (isPressed) {
+            this.#kstat = this.#kstat | KEY_MASK_LEFT;
+        } else {
+            this.#kstat = this.#kstat & ~(KEY_MASK_LEFT);
+        }
+    }
+
+    setKeyRightStatus (isPressed) {
+        if (isPressed) {
+            this.#kstat = this.#kstat | KEY_MASK_RIGHT;
+        } else {
+            this.#kstat = this.#kstat & ~(KEY_MASK_RIGHT);
+        }
+    }
+
+    setKeyAStatus (isPressed) {
+        if (isPressed) {
+            this.#kstat = this.#kstat | KEY_MASK_A;
+        } else {
+            this.#kstat = this.#kstat & ~(KEY_MASK_A);
+        }
+    }
+
+    setKeyBStatus (isPressed) {
+        if (isPressed) {
+            this.#kstat = this.#kstat | KEY_MASK_B;
+        } else {
+            this.#kstat = this.#kstat & ~(KEY_MASK_B);
+        }
+    }
+
+    isExited () {
+        return this.#exited;
     }
 }
